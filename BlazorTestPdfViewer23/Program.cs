1838001ef5d01167c0d8498198e9fc51564a7698
julien gourdon <julien.gourdon@live.fr>
2023-11-08 23:22:08 +0100

    Ajoutez des fichiers projet.

---
+++ BlazorTestPdfViewer23/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor().AddHubOptions(o => { o.MaximumReceiveMessageSize = 102400000; });
+// Add Syncfusion Blazor service to the container.
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mjc2MjgwOEAzMjMzMmUzMDJlMzBFNkJBWW92bjdUaXUyOElxMkg1d0RDQXZOdXUxS0pMR3NHTGpuazllSXJBPQ==");
+builder.Services.AddSyncfusionBlazor();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
