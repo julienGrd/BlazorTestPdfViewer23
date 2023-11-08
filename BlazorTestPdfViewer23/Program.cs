7cc4e2e3c77fd465a2380ebb36ee569b4a57e709
julien gourdon <julien.gourdon@live.fr>
2023-11-08 23:22:41 +0100

    suppr licence

--- BlazorTestPdfViewer23/Program.cs
+++ BlazorTestPdfViewer23/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor().AddHubOptions(o => { o.MaximumReceiveMessageSize = 102400000; });
 // Add Syncfusion Blazor service to the container.
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mjc2MjgwOEAzMjMzMmUzMDJlMzBFNkJBWW92bjdUaXUyOElxMkg1d0RDQXZOdXUxS0pMR3NHTGpuazllSXJBPQ==");
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("todo");
 builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
